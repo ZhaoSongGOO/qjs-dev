e77de35c359c365ca94ec109493810ec775ca7cd
zhaosonggo <zhaosonggo@gmail.com>
2025-02-17 17:48:39 +0800

    call js function js c++
    Change-Id: I31308d4fc747f02a4e929b98e6802617132f770f

--- demo.c
+++ demo.c
@@ -2,6 +2,7 @@
 
 #include "quickjs-libc.h"
 #include "cutils.h"
+#include "quickjs.h"
 
 
 static int eval_buf(JSContext *ctx, const void *buf, int buf_len,
@@ -69,6 +70,33 @@ int main(int argc, char **argv)
   ctx = JS_NewContext(rt);
   js_std_add_helpers(ctx, argc, argv);
   eval_file(ctx, "./demo.js", 0);
+
+  JSAtom atom = JS_NewAtom(ctx, "CustomFunction");
+  JSValue global_obj = JS_GetGlobalObject(ctx);
+
+  JSValue v = JS_GetProperty(ctx, global_obj, atom); 
+
+  if(JS_IsFunction(ctx, v)){
+    JSValue arg[1] = {JS_NewString(ctx, "Hello World!")};
+    JSValue ret = JS_Call(ctx, v, JS_UNDEFINED, 1, arg);
+    if (JS_IsException(ret)) {
+        // 调用失败，打印错误信息
+        js_std_dump_error(ctx);
+    } else {
+        // 调用成功，处理返回值
+        const char *str = JS_ToCString(ctx, ret);
+        printf("Function returned: %s\n", str);
+        JS_FreeCString(ctx, str);
+    }
+    JS_FreeValue(ctx, arg[0]);
+    JS_FreeValue(ctx, ret);
+  }
+
+
+
+  JS_FreeValue(ctx, v);
+  JS_FreeValue(ctx, global_obj);
+  JS_FreeAtom(ctx, atom);
   js_std_loop(ctx);
   js_std_free_handlers(rt);
   JS_FreeContext(ctx);
--- demo.js
+++ demo.js
@@ -1,3 +1,10 @@
 console.log("12345");
 
 song.log("hello");
+
+
+function CustomFunction(content){
+    console.log("print from CustomFunction:\n");
+    song.log(content);
+    return "call success!";
+}
