057b6ca07f9e082f630cdde82c9404cb9aa467ad
zhaosonggo <zhaosonggo@gmail.com>
2025-08-23 00:04:45 +0800

    增加 jni.h 的相关实现，包括 JSIObject, JSIFuction
    Change-Id: If03d37f659de5eec091b3e225402a609060b4c12

--- quickjs_app.cc
+++ quickjs_app.cc
@@ -3,54 +3,86 @@
 #include <map>
 #include "jsi.h"
 
-JSClassID JSIFunction::class_id_ = 0;
-std::map<JSRuntime*, bool> JSIFunction::initialized_runtimes_;
+JSClassID JSIApp::class_id_ = 0;
+std::map<JSRuntime *, bool> JSIApp::initialized_runtimes_;
 
-    JSIFunction::JSIFunction(JSContext * ctx):ctx_(ctx){
-        ensureInitialized(ctx);
-    }
+JSIApp::JSIApp(JSContext *ctx) : ctx_(ctx) {
+    ensureInitialized(ctx);
+}
 
-    JSValue JSIFunction::create(JSCallback cpp_func) {
-        JSValue func_obj = JS_NewObjectClass(ctx_, class_id_);
-        if (JS_IsException(func_obj)) {
-            return func_obj;
-        }
-        JSCallback* callback_ptr = new JSCallback(std::move(cpp_func));
-        JS_SetOpaque(func_obj, callback_ptr);
-        return func_obj;
+JSValue JSIApp::create(JSIObject *object) {
+    JSValue obj = JS_NewObjectClass(ctx_, class_id_);
+    if (JS_IsException(obj)) {
+        return obj;
     }
-    void JSIFunction::setGlobal(const std::string& name, JSCallback cpp_func) {
-        JSValue global_obj = JS_GetGlobalObject(ctx_);
-        JSValue func_val = create(std::move(cpp_func));
-        JS_SetPropertyStr(ctx_, global_obj, name.c_str(), func_val);
-        JS_FreeValue(ctx_, global_obj);
-    }
+    JS_SetOpaque(obj, object);
+    return obj;
+}
 
-    void JSIFunction::finalizer(JSRuntime *rt, JSValue val) {
-        JSCallback* callback = (JSCallback*)JS_GetOpaque(val, class_id_);
-        if (callback) {
-            delete callback;
-        }
-    }
-    JSValue JSIFunction::callAdapter(JSContext *ctx, JSValueConst func_obj,
-                               JSValueConst this_val, int argc, JSValueConst *argv,
-                               int flags) {
-        JSCallback* callback = (JSCallback*)JS_GetOpaque(func_obj, class_id_);
-        if (callback && (*callback)) {
-            return (*callback)(ctx, func_obj, this_val, argc, argv, flags);
-        }
-        return JS_EXCEPTION;
-    }
+void JSIApp::setGlobal(const std::string &name, JSIObject *object) {
+    JSValue global_obj = JS_GetGlobalObject(ctx_);
+    JSValue obj = create(object);
+    JS_SetPropertyStr(ctx_, global_obj, name.c_str(), obj);
+    JS_FreeValue(ctx_, global_obj);
+}
 
-    void JSIFunction::ensureInitialized(JSContext * context) {
-        JSRuntime * rt = JS_GetRuntime(context);
-        if (initialized_runtimes_.find(rt) == initialized_runtimes_.end()) {
-            JS_NewClassID(&class_id_);
-            JSClassDef class_def = {};
-            class_def.class_name = "CppFunction";
-            class_def.finalizer = &JSIFunction::finalizer;
-            class_def.call = &JSIFunction::callAdapter;
-            JS_NewClass(rt, class_id_, &class_def);
-            initialized_runtimes_[rt] = true;
-        }
+void JSIApp::finalizer(JSRuntime *rt, JSValue val) {
+    JSIObject *object = (JSIObject *) JS_GetOpaque(val, class_id_);
+    if (object) {
+        delete object;
     }
+}
+
+JSValue JSIApp::callAdapter(JSContext *ctx, JSValueConst func_obj,
+                            JSValueConst this_val, int argc, JSValueConst *argv,
+                            int flags) {
+    JSIObject *object = (JSIObject *) JS_GetOpaque(func_obj, class_id_);
+    if (object) {
+        return object->call(ctx, func_obj, this_val, argc, argv, flags);
+    }
+    return JS_EXCEPTION;
+}
+
+JSValue JSIApp::getAdapter(JSContext *ctx, JSValue obj, JSAtom prop, JSValue receiver) {
+    JSIObject *object = (JSIObject *) JS_GetOpaque(obj, class_id_);
+    if (object) {
+        return object->get(ctx, obj, prop, receiver);
+    }
+    return JS_EXCEPTION;
+}
+
+int JSIApp::setAdapter(JSContext *ctx, JSValue obj, JSAtom prop, JSValue value, JSValue receiver, int flags) {
+    JSIObject *object = (JSIObject *) JS_GetOpaque(obj, class_id_);
+    if (object) {
+        return object->set(ctx, obj, prop, value, receiver, flags);
+    }
+    return 0;
+}
+
+void JSIApp::ensureInitialized(JSContext *context) {
+    JSRuntime *rt = JS_GetRuntime(context);
+    static JSClassExoticMethods exotic = {
+            .get_property = &JSIApp::getAdapter,
+            .set_property = &JSIApp::setAdapter,
+    };
+    static JSClassDef class_def = {
+            .class_name = "_native",
+            .finalizer = &JSIApp::finalizer,
+            .call = &JSIApp::callAdapter,
+            .exotic = &exotic
+    };
+    if (initialized_runtimes_.find(rt) == initialized_runtimes_.end()) {
+        JS_NewClassID(&class_id_);
+        JS_NewClass(rt, class_id_, &class_def);
+        initialized_runtimes_[rt] = true;
+    }
+}
+
+JSValue JSIFunction::call(JSContext *ctx, JSValueConst func_obj,
+                          JSValueConst this_val, int argc, JSValueConst *argv,
+                          int flags) {
+    if (callback_ == nullptr) {
+        return JS_UNDEFINED;
+    }
+    return (*callback_)(ctx, func_obj, this_val, argc, argv, flags);
+}
